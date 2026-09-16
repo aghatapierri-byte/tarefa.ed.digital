fcce333249f6b5c473417523329d2c2965d56d20
aghatapierri-byte <aghata.pierri@escola.pr.gov.br>
2026-09-16 13:43:18 -0300

    Atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botoesCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("spam");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;}
+            else{
+                contador.textContent--;
+                curtiu = false;
+            }
